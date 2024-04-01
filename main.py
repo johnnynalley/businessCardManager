acf2ca0e2aa7b1e1545549f5d567293f4ede4391
Johnny Nalley <72593738+johnnynalley@users.noreply.github.com>
2024-04-01 09:25:36 -0500

    added save_business_card

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+import tkinter.filedialog
 from tkinter import filedialog, Tk
 from PIL import Image
-import pytesseract
+import pytesseract, shutil
 
 root = Tk()
 root.withdraw()
 
+cards = []
 
-
-
-def get_card():
-    # Gets the path of the desired business card
+# Saves the currently open business card
+def save_business_card():
     while True:
         try:
-            path = filedialog.askopenfilename()
-            break
+            input("Press Enter to save the businessCard ")
+            businessCard = tkinter.filedialog.asksaveasfilename()
+            return businessCard
 
         except IsADirectoryError:
             input("Press Enter to try again. ")
 
+    # Gets the path of the desired business card
+def open_business_card():
+
 # Gets the text from the selected image
 def get_card_text():
-    imageText = pytesseract.image_to_string(Image.open(path))
+    imageText = pytesseract.image_to_string(Image.open())
 
     if len(imageText) == 0:
         return "I was unable to find any text in the image."
